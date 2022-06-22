f96c6f5ceb86fd309955d1922cf126a31df92df3
Martin Kilmartin <martinkilmartin@gmail.com>
2022-06-22 10:43:09 -0400

    Fix for delete button not showing

--- components/Message.js
+++ components/Message.js
@@ -9,22 +9,23 @@ const Message = ({ message }) => {
 
   return (
     <Box>
-      <Grid templateColumns="repeat(7, 1fr)" gap={4}>
-        <GridItem colSpan={2} >
+      <Grid templateColumns='repeat(7, 1fr)' gap={4}>
+        <GridItem colSpan={2}>
           <Text>{message.author.username}</Text>
         </GridItem>
-        <GridItem colSpan={4} >
+        <GridItem colSpan={4}>
           <Text>{message.message}</Text>
         </GridItem>
-        <GridItem colSpan={1} >
-          {user?.id === message.user_id ||
-            (userRoles.some((role) =>
+        <GridItem colSpan={1}>
+          {user?.id === message.user_id ? (
+            <button onClick={() => deleteMessage(message.id)}>🚮</button>
+          ) : userRoles.some((role) =>
               ["admin", "moderator"].includes(role)
-            ) && (
-              <button onClick={() => deleteMessage(message.id)}>
-                <TrashIcon />
-              </button>
-            ))}
+            ) ? (
+            <button onClick={() => deleteMessage(message.id)}>🗑️</button>
+          ) : (
+            ""
+          )}
         </GridItem>
       </Grid>
     </Box>
