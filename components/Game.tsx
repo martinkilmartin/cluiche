b25d188a7aa673cbd1a76cc6009fbf4b343de079
Martin Kilmartin <martinkilmartin@gmail.com>
2022-06-23 12:06:13 -0400

    Adds some game play

--- components/Game.tsx
+++ components/Game.tsx
@@ -26,6 +26,111 @@ import { euroCountries } from "@constants/euro-countries";
 
 const MAX_Q = 10;
 
+const usaEuSorted = sortData();
+
+function sortData() {
+  const usaSorted = usaStates.sort(
+    (a, b) => (a[1] as number) - (b[1] as number)
+  );
+  usaSorted.forEach((usa) => usa.length === 2 && usa.push("🇺🇸"));
+
+  const euSorted = euroCountries.sort(
+    (a, b) => (a[1] as number) - (b[1] as number)
+  );
+  euSorted.forEach((eu) => eu.length === 2 && eu.push("🇪🇺"));
+
+  const usaEuMerged = [...usaSorted, ...euSorted];
+
+  const usaEuSorted = usaEuMerged.sort(
+    (a, b) => (a[1] as number) - (b[1] as number)
+  );
+  return usaEuSorted;
+}
+
+function createQuestions() {
+  const qashes = new Set();
+  const qArray = new Array<Array<string>>();
+  const answers = new Array<string>();
+  let i = 0;
+  while (qArray.length < MAX_Q && i < usaEuSorted.length) {
+    const rando = Math.floor(Math.random() * usaEuSorted.length);
+    //console.log(usaEuSorted[rando][0]);
+    //console.log(findSmaller(rando, usaEuSorted[rando][2] as "🇺🇸" | "🇪🇺"));
+    const smaller = findSmaller(rando, usaEuSorted[rando][2] as "🇺🇸" | "🇪🇺");
+    const larger = findLarger(rando, usaEuSorted[rando][2] as "🇺🇸" | "🇪🇺");
+    if (smaller && larger) {
+      const flag = usaEuSorted[rando][2];
+      const q = `What ${
+        flag === "🇺🇸" ? "European country" : "US State (or Territory)"
+      } is closest is size, either larger or smaller than ${
+        usaEuSorted[rando][0]
+      }?`;
+      const a = `${larger[0]} is larger by ${
+        larger[1] - (usaEuSorted[rando][1] as number)
+      } and ${smaller[0]} is smaller by ${
+        (usaEuSorted[rando][1] as number) - smaller[1]
+      }.`;
+      if (!qashes.has(q)) {
+        qArray.push([q, a, `${larger[0]},${smaller[0]}`]);
+        qashes.add(q);
+        answers.push();
+      }
+    } else if (smaller || larger) {
+      if (smaller) {
+        const flag = usaEuSorted[rando][2];
+        const q = `What ${
+          flag === "🇺🇸" ? "European country" : "US State (or Territory)"
+        } is closest is size, but smaller than ${usaEuSorted[rando][0]}?`;
+        const a = `${smaller[0]}.`;
+        if (!qashes.has(q)) {
+          qArray.push([q, a, smaller[0]]);
+          qashes.add(q);
+        }
+      }
+      if (larger) {
+        const flag = usaEuSorted[rando][2];
+        const q = `What ${
+          flag === "🇺🇸" ? "European country" : "US State (or Territory)"
+        } is closest is size, but larger than ${usaEuSorted[rando][0]}?`;
+        const a = `${larger[0]}.`;
+        if (!qashes.has(q)) {
+          qArray.push([q, a, larger[0]]);
+          qashes.add(q);
+        }
+      }
+    }
+    i++;
+  }
+  return qArray;
+}
+
+function findSmaller(
+  index: number,
+  flag: "🇺🇸" | "🇪🇺"
+): null | [string, number, "🇺🇸" | "🇪🇺"] {
+  if (index < 1) {
+    return null;
+  } else if (usaEuSorted[index - 1][2] !== flag) {
+    return usaEuSorted[index - 1] as [string, number, "🇺🇸" | "🇪🇺"];
+  } else {
+    return findSmaller(index - 1, flag);
+  }
+}
+
+function findLarger(
+  index: number,
+  flag: "🇺🇸" | "🇪🇺"
+): null | [string, number, "🇺🇸" | "🇪🇺"] {
+  if (index >= usaEuSorted.length - 1) {
+    return null;
+  } else if (usaEuSorted[index + 1][2] !== flag) {
+    return usaEuSorted[index + 1] as [string, number, "🇺🇸" | "🇪🇺"];
+  } else {
+    return findLarger(index + 1, flag);
+  }
+}
+const qArray = createQuestions();
+
 type GameProps = {
   user: User;
   data: UserAccountType | undefined;
@@ -34,21 +139,23 @@ const Game = ({ user, data }: GameProps): JSX.Element => {
   const [game, setGame] = useState<Game | null>(null);
   const [buyIn, setBuyIn] = useState(0);
   const [newGame, setNewGame] = useState("");
-  const [errorText, setError] = useState("");
-  const [questions, setQuestions] = useState([["", ""]]);
+  const [errorText, setError] = useState<string | null>(null);
+  const [successText, setSuccess] = useState<string | null>(null);
+  const [newAnswer, setNewAnswer] = useState("");
+  const [qIndex, setQIndex] = useState(0);
 
-  useEffect(() => {
-    fetchGame();
-  }, []);
+  // useEffect(() => {
+  //   fetchGame();
+  // }, []);
 
-  const fetchGame = async () => {
-    const { data: games, error } = await supabase
-      .from("games")
-      .select("*")
-      .order("created_at", { ascending: true });
-    if (error) console.error("error", error);
-    else setGame(games[0]);
-  };
+  // const fetchGame = async () => {
+  //   const { data: games, error } = await supabase
+  //     .from("games")
+  //     .select("*")
+  //     .order("created_at", { ascending: true });
+  //   if (error) console.error("error", error);
+  //   else setGame(games[0]);
+  // };
 
   function buyInWrap(n: number) {
     setBuyIn(n);
@@ -74,156 +181,127 @@ const Game = ({ user, data }: GameProps): JSX.Element => {
     if (error) console.error("error", error);
     else console.log(data);
   }
-  const usaSorted = usaStates.sort(
-    (a, b) => (a[1] as number) - (b[1] as number)
-  );
-  usaSorted.forEach((usa) => usa.length === 2 && usa.push("🇺🇸"));
-  //console.log(usaSorted);
-  const euSorted = euroCountries.sort(
-    (a, b) => (a[1] as number) - (b[1] as number)
-  );
-  euSorted.forEach((eu) => eu.length === 2 && eu.push("🇪🇺"));
-  //console.log(euSorted);
 
-  const usaEuMerged = [...usaSorted, ...euSorted];
-  //console.log(usaEuMerged);
-
-  const usaEuSorted = usaEuMerged.sort(
-    (a, b) => (a[1] as number) - (b[1] as number)
-  );
-  //console.log(usaEuSorted);
-
-  function createQuestions() {
-    const qashes = new Set();
-    const qArray = new Array<Array<string>>();
-    let i = 0;
-    while (qArray.length < MAX_Q && i < usaEuSorted.length) {
-      const rando = Math.floor(Math.random() * usaEuSorted.length);
-      //console.log(usaEuSorted[rando][0]);
-      //console.log(findSmaller(rando, usaEuSorted[rando][2] as "🇺🇸" | "🇪🇺"));
-      const smaller = findSmaller(rando, usaEuSorted[rando][2] as "🇺🇸" | "🇪🇺");
-      if (smaller) {
-        const flag = usaEuSorted[rando][2];
-        const q = `What ${
-          flag === "🇺🇸" ? "European country" : "US State (or Territory)"
-        } is closest is size, but smaller than ${usaEuSorted[rando][0]}?`;
-        const a = `${smaller[0]}.`;
-        if (!qashes.has(q)) {
-          qArray.push([q, a]);
-          qashes.add(q);
-        }
+  const addAnswer = (a: string) => {
+    setError(null);
+    setSuccess(null);
+    const myAnswer = a.trim().toLowerCase();
+    const possibleAnswers = qArray[qIndex][2]
+      .split(",")
+      .map((e) => e.toLowerCase());
+    console.log(myAnswer);
+    console.log(possibleAnswers);
+    if (possibleAnswers.includes(myAnswer)) {
+      setSuccess(`${myAnswer} is correct! Well done!`);
+      if (qIndex + 1 < qArray.length) {
+        setQIndex(qIndex + 1);
       }
-      i++;
-    }
-    return qArray;
-  }
-
-  function findSmaller(
-    index: number,
-    flag: "🇺🇸" | "🇪🇺"
-  ): null | [string, number, "🇺🇸" | "🇪🇺"] {
-    if (index < 1) {
-      return null;
-    } else if (usaEuSorted[index - 1][2] !== flag) {
-      return usaEuSorted[index - 1] as [string, number, "🇺🇸" | "🇪🇺"];
     } else {
-      return findSmaller(index - 1, flag);
+      setError(`${myAnswer} is wrong! ❌!`);
     }
-  }
+  };
 
-  console.log(createQuestions());
+  const giveUp = () => {
+    setError(null);
+    setSuccess(qArray[qIndex][1]);
+    if (qIndex + 1 < qArray.length) {
+      setQIndex(qIndex + 1);
+    }  
+  }
 
   return (
-    <Box w="100%">
+    <Box w='100%'>
       <Container centerContent>
         <Heading>Your Game Name</Heading>
         <Text>Some minor blah blah blah</Text>
         <Wrap p={5}>
           <WrapItem>
-            <Avatar name="shark" src="/avatars/svg/050-shark.svg">
+            <Avatar name='shark' src='/avatars/svg/050-shark.svg'>
               <AvatarBadge
-                borderColor="greenyellow"
-                boxSize="1em"
-                bg="green.500"
+                borderColor='greenyellow'
+                boxSize='1em'
+                bg='green.500'
               />
             </Avatar>
           </WrapItem>
           <WrapItem>
-            <Avatar name="frog" src="/avatars/svg/049-frog.svg">
-              <AvatarBadge bg="green.500" boxSize="1em" />
+            <Avatar name='frog' src='/avatars/svg/049-frog.svg'>
+              <AvatarBadge bg='green.500' boxSize='1em' />
             </Avatar>
           </WrapItem>
           <WrapItem>
-            <Avatar name="worker" src="/avatars/svg/048-worker.svg">
-              <AvatarBadge borderColor="papayawhip" bg="tomato" boxSize="1em" />
+            <Avatar name='worker' src='/avatars/svg/048-worker.svg'>
+              <AvatarBadge borderColor='papayawhip' bg='tomato' boxSize='1em' />
             </Avatar>
           </WrapItem>
           <WrapItem>
-            <Avatar name="zebra" src="/avatars/svg/047-zebra.svg">
-              <AvatarBadge borderColor="papayawhip" bg="tomato" boxSize="1em" />
+            <Avatar name='zebra' src='/avatars/svg/047-zebra.svg'>
+              <AvatarBadge borderColor='papayawhip' bg='tomato' boxSize='1em' />
             </Avatar>
           </WrapItem>
           <WrapItem>
-            <Avatar name="vampire" src="/avatars/svg/046-vampire.svg">
-              <AvatarBadge borderColor="papayawhip" bg="tomato" boxSize="1em" />
+            <Avatar name='vampire' src='/avatars/svg/046-vampire.svg'>
+              <AvatarBadge borderColor='papayawhip' bg='tomato' boxSize='1em' />
             </Avatar>
           </WrapItem>
           <WrapItem>
-            <Avatar name="giraffe" src="/avatars/svg/045-giraffe.svg">
-              <AvatarBadge borderColor="papayawhip" bg="tomato" boxSize="1em" />
+            <Avatar name='giraffe' src='/avatars/svg/045-giraffe.svg'>
+              <AvatarBadge borderColor='papayawhip' bg='tomato' boxSize='1em' />
             </Avatar>
           </WrapItem>
           <WrapItem>
-            <Avatar name="officer" src="/avatars/svg/044-officer.svg">
-              <AvatarBadge borderColor="papayawhip" bg="tomato" boxSize="1em" />
+            <Avatar name='officer' src='/avatars/svg/044-officer.svg'>
+              <AvatarBadge borderColor='papayawhip' bg='tomato' boxSize='1em' />
             </Avatar>
           </WrapItem>
         </Wrap>
       </Container>
       <Flex
-        bg="#edf3f8"
+        bg='#edf3f8'
         _dark={{
           bg: "#3e3e3e",
         }}
         p={5}
-        alignItems="center"
-        justifyContent="center"
+        alignItems='center'
+        justifyContent='center'
       >
         <Box
-          mx="auto"
+          mx='auto'
           px={8}
           py={4}
-          rounded="lg"
-          shadow="lg"
-          bg="white"
+          rounded='lg'
+          shadow='lg'
+          bg='white'
           _dark={{
             bg: "gray.800",
           }}
         >
-          <Flex minWidth="max-content" alignItems="center" gap="2">
+          <Flex minWidth='max-content' alignItems='center' gap='2'>
             <chakra.span
-              fontSize="md"
-              color="gray.600"
+              fontSize='md'
+              color='gray.600'
               _dark={{
                 color: "gray.400",
               }}
             >
-              Question 9 of 10
+              {qArray?.length && `Question ${qIndex + 1} of ${qArray?.length}`}
             </chakra.span>
             <Spacer />
             <Stat>
-              <StatNumber>00:59</StatNumber>
+              <StatNumber>
+                {(errorText && errorText) || (successText && successText)}
+              </StatNumber>
             </Stat>
           </Flex>
 
           <Box mt={2}>
             <Text
-              fontSize="2xl"
-              color="gray.700"
+              fontSize='2xl'
+              color='gray.700'
               _dark={{
                 color: "white",
               }}
-              fontWeight="700"
+              fontWeight='700'
               _hover={{
                 color: "gray.600",
                 _dark: {
@@ -232,19 +310,25 @@ const Game = ({ user, data }: GameProps): JSX.Element => {
                 textDecor: "underline",
               }}
             >
-              What year was the very first model of the iPhone released?
+              {qArray[qIndex][0]}
             </Text>
             <FormControl isRequired mt={2}>
-              <FormLabel htmlFor="answer">Answer</FormLabel>
-              <Input id="answer" placeholder="Enter you answer here" />
+              <FormLabel htmlFor='answer'>Answer</FormLabel>
+              <Input
+                id='answer'
+                placeholder='Enter you answer here'
+                onChange={(e) => {
+                  setNewAnswer(e.target.value);
+                }}
+              />
             </FormControl>
           </Box>
-          <Flex justifyContent="space-between" alignItems="center" mt={4}>
-            <Button aria-label="Vote Up" fontSize={"2xl"} colorScheme={"green"}>
+          <Flex justifyContent='space-between' alignItems='center' mt={4}>
+            <Button aria-label='Vote Up' fontSize={"2xl"} colorScheme={"green"}>
               👍
             </Button>
             <Button
-              aria-label="Vote Down"
+              aria-label='Vote Down'
               fontSize={"2xl"}
               ml={2}
               colorScheme={"red"}
@@ -252,7 +336,12 @@ const Game = ({ user, data }: GameProps): JSX.Element => {
               👎
             </Button>
             <Spacer />
-            <Button colorScheme="blue">Next</Button>
+            <Button colorScheme='blue' onClick={() => giveUp()}>
+              Give Up
+            </Button>
+            <Button colorScheme='blue' onClick={() => addAnswer(newAnswer)}>
+              Next
+            </Button>
           </Flex>
         </Box>
       </Flex>
